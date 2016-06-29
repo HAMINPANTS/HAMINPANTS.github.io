689ce53d15a9901b5b540b9bb5ee6b31d395f0bd
Sam <samtfong@hotmail.com>
2016-06-29 15:30:45 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-var thing = 1;
-if(thing == 1){
-  confirm("Is this working?");
+var main = function(){
+  var thing = 1;
+  if(thing == 1){
+    confirm("Is this working?");
+  };
+  $('#title').fadeOut(600);
 };
